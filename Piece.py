53d93944a5805ffc05ec8a94273ce0c654b6602e
idopo <idopongo@gmaill.com>
2023-03-23 13:07:47 +0100

    knight can now no longer eat pieces 2 steps below it.

--- Piece.py
+++ Piece.py
@@ -163,7 +163,7 @@ class Knight(Piece):
         row_change = [-2, -2, -1, -1, +1, +1, +2, +2]
         col_change = [-1, +1, -2, +2, -2, +2, +1, -1]
 
-        for i in range(0, 8):
+        for i in range(0, 6):
             new_row = self.get_row_number() + row_change[i]
             new_col = self.get_col_number() + col_change[i]
             evaluating_square = game_state.get_piece(new_row, new_col)
