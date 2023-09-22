6374db89d522de7367411af9a7aa2f28cf709208
Ty Chermsirivatana <professornitro@gmail.com>
2023-09-22 01:22:55 -0400

    Update knight.py

--- pychess/knight.py
+++ pychess/knight.py
@@ -1,29 +1,23 @@
-
 from enums import Player
 from piece import Piece
 
-
 class Knight(Piece):
-    # Get moves
     def get_valid_piece_moves(self, game_state):
         _peaceful_moves = []
         _piece_takes = []
 
-        row_change = [-2, -2, -1, -1, +1, +1, +2, +2]
-        col_change = [-1, +1, -2, +2, -2, +2, +1, -1]
+        moves = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)]
 
-        for i in range(0, 8):
-            new_row = self.get_row_number() + row_change[i]
-            new_col = self.get_col_number() + col_change[i]
+        for move in moves:
+            new_row = self.get_row_number() + move[0]
+            new_col = self.get_col_number() + move[1]
 
-            if not 0 <= new_row < 8 or not 0 <= new_col < 8:
-                continue
+            if 0 <= new_row < 8 and 0 <= new_col < 8:
+                evaluating_square = game_state.get_piece(new_row, new_col)
+                
+                if evaluating_square == Player.EMPTY:
+                    _peaceful_moves.append((new_row, new_col))
+                elif not evaluating_square.is_player(self.get_player()):
+                    _piece_takes.append((new_row, new_col))
 
-            evaluating_square = game_state.get_piece(new_row, new_col)
-            
-            if evaluating_square == Player.EMPTY:
-                _peaceful_moves.append((new_row, new_col))
-            elif not evaluating_square.is_player(self.get_player()):
-                _piece_takes.append((new_row, new_col))
-        
         return _peaceful_moves + _piece_takes
